7b86c7da561f3b71efaca0d0798cda8e58d86d60
yun <yunhy3721@naver.com>
2016-09-09 11:38:14 +0900

    first html for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'yunsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'', include('blog.urls')),
+    url(r'^$', views.post_list, name='post_list'),
 ]
