0405b01990ad7eb02bdfe6d841ce751af2ecf4a7
yun <yunhy3721@naver.com>
2016-09-12 09:58:22 +0900

    Added view and template

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
