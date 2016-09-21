89a708f1c989562eed0b921b23f5413c3bddff48
yun <yunhy3721@naver.com>
2016-09-21 16:23:54 +0900

    Added view and template

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [ url(r'^$', views.mypost, name='mypost'), ]
+urlpatterns = [ 
+	url(r'^$', views.mypost, name='mypost'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+]
