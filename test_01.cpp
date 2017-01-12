7d121e1a8764cb73eeceaf9ca010d9f451363aed
hy.yun <hy.yun@lge.com>
2017-01-12 14:16:15 +0900

    add int

--- test_01.cpp
+++ test_01.cpp
@@ -2,16 +2,20 @@
 #include <stdio>
 #include <stdlib>
 
+#include <vector>
+
 #define BufferSize 2048
 
 using namespace std;
 
 int main()
 {
-    int Buffer = BufferSize;
-    
 	cout<< "Test_01" <<endl;
     cout<< BufferSize <<endl;
+
+    cout << "good!" <<endl;
+
+    int i = 10;
 
     return 0;
 }
