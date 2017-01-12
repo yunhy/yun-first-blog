74f6b7251da219a89fe9a60d62302ceaadb91f57
hy.yun <hy.yun@lge.com>
2017-01-12 11:11:44 +0900

    add include n define

--- test.cpp
+++ test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <stdio>
+#include <stdlib>
+
+#define BufferSize 2048
 
 using namespace std;
 
 int main()
 {
 	cout<< "Test_01" <<endl;
+    cout<< BufferSize <<endl;
 
     return 0;
 }
